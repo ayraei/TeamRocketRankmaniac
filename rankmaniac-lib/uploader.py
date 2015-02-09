a245a8264fc9d978f4fa247df1bd023ce6852796
gankun <cedeno_matthew@hotmail.com>
2015-02-08 18:11:18 -0800

    Added a function to get the transition matrix from the adj list.

--- rankmaniac-lib/uploader.py
+++ rankmaniac-lib/uploader.py
@@ -95,6 +95,33 @@ def do_main(team_id, access_key, secret_key,
         print('  Downloaded')
 
     print('Terminated')
+    
+def get_P(adjList):
+    """ Function to read the adj. list of a graph and return the transition
+    matrix. 
+    
+    @param adjList list of strings in the format of NodeId:(node#)\tpagerank,
+    oldpagerank, other node #s,
+    example:
+    NodeId:0\t1.0,0.0,1,3
+    
+    @ return returns a list of lists. Each element is a row in the trans 
+    matrix. Sum of each row is 1.
+    """
+    P = []
+    n = len(adjList)
+    
+    # get row for each node in the graph
+    for node in adjList:
+        
+        row = [0] * n
+        # get list of other nodes it connects to
+        edges = node.split("\t")[1].split(",")[2:]
+        # update values in row to be 1/n if node is connected
+        for edg in edges:
+            row[int(edg)] = (1.0 / len(edges))
+        P.append(row)
+    return P 
 
 if __name__ == '__main__':
 
