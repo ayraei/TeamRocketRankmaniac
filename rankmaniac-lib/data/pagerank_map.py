e9107845cb9940d364dea1b6cf7c3c323677d72a
Joanne Li <ljoanne7@gmail.com>
2015-02-09 23:01:34 -0800

    Some more parser code written in process_map.py\n\nBoth reducer functions should be unmodified for Early Start

--- rankmaniac-lib/data/pagerank_map.py
+++ rankmaniac-lib/data/pagerank_map.py
@@ -3,37 +3,16 @@
 import sys
 
 #
-# This program simply represents the identity function.
+# This program generates the transition matrix and performs matrix
+# multiplication on the distribution vector
 #
-
-def get_P(adjList):
-    """ Function to read the adj. list of a graph and return the transition
-    matrix. 
-    
-    @param adjList list of strings in the format of NodeId:(node#)\tpagerank,
-    oldpagerank, other node #s,
-    example:
-    NodeId:0\t1.0,0.0,1,3
-    
-    @ return returns a list of lists. Each element is a row in the trans 
-    matrix. Sum of each row is 1.
-    """
-    P = []
-    n = len(adjList)
-    
-    # get row for each node in the graph
-    for node in adjList:
-        
-        row = [0] * n
-        # get list of other nodes it connects to
-        edges = node.split("\t")[1].split(",")[2:]
-        # update values in row to be 1/n if node is connected
-        for edg in edges:
-            row[int(edg)] = (1.0 / len(edges))
-        P.append(row)
-    return P
 
 adjList = []
 trans = []
 for line in sys.stdin:
-    sys.stdout.write(line)
+    # Use the first pass of MapReduce to generate adjacency matrix
+    # and rewrite data into desired format
+    if True:
+        adjList.append(line)
+    else:
+        sys.stdout.write(line)
--- rankmaniac-lib/data/process_map.py
+++ rankmaniac-lib/data/process_map.py
@@ -3,9 +3,57 @@
 import sys
 
 #
-# This program simply represents the identity function.
+# This program does things.
 #
 
-for line in sys.stdin:
-    sys.stdout.write(line)
+def get_P(adjList):
+    """ Function to read the adj. list of a graph and return the transition
+    matrix. 
+    
+    @param adjList list of strings in the format of NodeId:(node#)\tpagerank,
+    oldpagerank, other node #s,
+    example:
+    NodeId:0\t1.0,0.0,1,3
+    
+    @ return returns a list of lists. Each element is a row in the trans 
+    matrix. Sum of each row is 1.
+    """
+    P = []
+    n = len(adjList)
+    
+    # get row for each node in the graph
+    for node in adjList:
+        
+        row = [0] * n
+        # get list of other nodes it connects to
+        edges = node.split("\t")[1].split(",")[2:]
+        # update values in row to be 1/n if node is connected
+        for edg in edges:
+            row[int(edg)] = (1.0 / len(edges))
+        P.append(row)
+    return P
 
+adjList = []
+colAdjList = []
+distrVector = []
+firstIteration = False
+for line in sys.stdin:
+    # Compute the transition matrix only on the first MapReduce iteration
+    if 'NodeId' in line:
+        firstIteration = True
+        adjList.append(line)
+    else:
+        nodeId,iterNum = line.split()[0].split(';')
+        colVal,adjCol = line.split()[1].split(';')
+        if len(distrVector) == 0:
+            distrVector = [0] * len(adjCol.split(','))
+            colAdjList = [0] * len(adjCol.split(','))
+        distrVector[nodeId] = colVal
+        colAdjList[nodeId] = adjCol
+
+if firstIteration:
+    trans = get_P(adjList)
+    for i in range(len(trans)):
+        sys.stdout.write(i)
+else:
+    pass
