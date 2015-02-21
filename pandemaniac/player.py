c88b9f81a7f77a00d0c9df68864d04419a65a946
gankun <cedeno_matthew@hotmail.com>
2015-02-21 15:35:02 -0800

    Updated the player script to have strategy 2, designed for the TA teams

--- pandemaniac/player.py
+++ pandemaniac/player.py
@@ -72,6 +72,41 @@ def basic_strategy_1(adj):
     
     return top[:NUM_SEEDS] * NUM_GAMES
 
+def basic_strategy_2(adj):
+    ''' Basic strategy example for pandemaniac. Other functions should follow
+    this format. Basic strategy 1 simply returns the top 5 nodes and the highest
+    degree neighbor for each of those nodes. This is designed to beat the TA 
+    team
+    
+    Input is an adjacency list in json format.
+    
+    Output is a list of 10 nodes to use as the seed nodes.'''
+    
+    deg_list = (get_degree_list(adj))
+    
+    top = get_top_list(deg_list)
+    
+    seeds = top[:NUM_SEEDS / 2]
+    
+    # get the highest degree neighbor for each top seed.
+    for i in range(5):
+        key = "%d" % i
+        max_D = 0
+        max_key = -1
+        # Loop to get the max neighbor
+        for neighbor in adj[key]:
+            if deg_list[int(neighbor)] > max_D and int(neighbor) not in seeds:
+                max_D = deg_list[int(neighbor)]
+                max_key = int(neighbor)
+            
+        # Error if no seed was found
+        if max_key == -1:
+            raise KeyError()
+        # Add the highest neighbor to the seeds list
+        seeds.append(max_key)
+    
+    return seeds * NUM_GAMES
+
 def go_for_top_3(adj):
     ''' Strategy where the pandemic will aim for the top three degree nodes.
     Assuming the competion will cancel out the top nodes, this strategy aims to
@@ -119,11 +154,15 @@ def go_for_top_3(adj):
 # Python Script
 
 # set the strategy to use
-STRATEGY = 0
+STRATEGY = 1
 NUM_GAMES = 1 # number of games we select nodes for
 NUM_SEEDS = 10 # number of seeds needed for each game
 
 json_file = sys.argv[1]
+
+# if a second argument is given use that strategy
+if len(sys.argv) > 2:
+    STRATEGY = sys.argv[2]
 
 # open the file and create an adj list
 data = []
@@ -136,7 +175,7 @@ seed = []
 # Pick which strategy to use
 
 if STRATEGY == 1:
-    seed = go_for_top_3(data[0])
+    seed = basic_strategy_2(data[0])
 
 elif STRATEGY == 2:
     pass
