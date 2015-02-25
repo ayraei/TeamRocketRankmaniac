7af9b409d68b7ac03914185db60ccfecdd5762b4
Joanne L <joanne@caltech.edu>
2015-02-24 23:37:59 -0800

    Finished 'supercluster' strategy and assigned to strategies 4(all neighbors) and 5(half neighbors)'

--- pandemaniac/supercluster.py
+++ pandemaniac/supercluster.py
@@ -1,17 +1,31 @@
-NUM_SEEDS = 20
-
-def supercluster1(adj, deg_list):
+def supercluster1(NUM_SEEDS, adj, deg_list):
+    '''Currently defaults to picking the top NUM_SEEDS highest degree
+    nodes.'''
     output = []
     output.append(deg_list[NUM_SEEDS])
     for i in range(NUM_SEEDS):
-        curr = output[i]
-        # get current node's neighbors
+        try:
+            curr = output[i]
+        except IndexError:
+            break
         neighbors = adj[str(curr)]
-        # votes needed if all nodes surrounding are colored (=total votes/2 rounded up)
+        if len(neighbors) == 0:
+            continue
         needed = int((len(adj[str(curr)]) + 1.5 + 1)/2)
-        
+        for j in range(min(needed, len(neighbors))):
+            try:
+                node = neighbors.pop()
+            except IndexError:
+                break
+            if int(node) not in output:
+                output.append(int(node))
+    for x in deg_list[:NUM_SEEDS]:
+        if x not in output:
+            output.append(x)
+    return output[:NUM_SEEDS]
 
-def supercluster2(adj, deg_list):
+
+def supercluster2(NUM_SEEDS, adj, deg_list):
     '''Currently defaults to picking the top NUM_SEEDS highest degree
     nodes.'''
     output = []
